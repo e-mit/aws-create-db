92a79401af7ac88ad33eab05e4df53fa690798b7
e <6410762+e-mit@users.noreply.github.com>
2024-03-09 14:13:09 +0000

    Make the allowed SSH IP address a template parameter

--- create.sh
+++ create.sh
@@ -5,6 +5,8 @@ export AWS_PAGER=""
 
 KEY_NAME=testKey
 STACK_NAME=testDBstack
+KEY_FILENAME=key.pem
+MY_IP=$(curl -4 icanhazip.com)
 
 if [[ -z $DB_PASSWORD ]]; then
     echo ERROR: DB_PASSWORD is not set
@@ -13,15 +15,16 @@ fi
 
 # Create new key pair and save the private key to file
 aws ec2 create-key-pair \
---key-name $KEY_NAME | jq -r '.KeyMaterial' > key.pem
-chmod go-rw key.pem
+--key-name $KEY_NAME | jq -r '.KeyMaterial' > $KEY_FILENAME
+chmod go-rw $KEY_FILENAME
 
 # Create the stack
 aws cloudformation create-stack \
 --stack-name $STACK_NAME \
 --template-body file://template.yml \
 --parameters ParameterKey=keyname,ParameterValue=$KEY_NAME \
-ParameterKey=DBPassword,ParameterValue=$DB_PASSWORD
+ParameterKey=DBPassword,ParameterValue=$DB_PASSWORD \
+ParameterKey=allowedIP,ParameterValue=$MY_IP
 
 read -p "Press any key to continue... " -n1 -s; echo
 
@@ -45,15 +48,18 @@ python3 -c "import sys, json
 print(json.load(sys.stdin)['Reservations'][0]['Instances'][0]['PublicIpAddress'])")
 
 # SSH to the EC2
-ssh -t -i key.pem -o StrictHostKeyChecking=accept-new \
+ssh -t -i $KEY_FILENAME -o StrictHostKeyChecking=accept-new \
     ec2-user@$EC2_IP \
     PGPASSWORD=$DB_PASSWORD DB_ENDPOINT=$DB_ENDPOINT bash -l
 
-# Then connect to the database:
-#sudo dnf update -y
-#sudo dnf install -y postgresql15 # NB: only version available
-#psql --host=$DB_ENDPOINT --port=5432 \
-#     --dbname=postgres --username=dbuser
+# Then connect to the database from the EC2:
+if (( 0==1 )); then
+    # Run this over SSH
+    sudo dnf update -y
+    sudo dnf install -y postgresql15 # NB: only version available
+    psql --host=$DB_ENDPOINT --port=5432 \
+        --dbname=postgres --username=dbuser
+fi
 
 read -p "Press any key to continue... " -n1 -s; echo
 
@@ -63,4 +69,4 @@ aws cloudformation delete-stack --stack-name $STACK_NAME
 aws ec2 delete-key-pair \
 --key-name $KEY_NAME
 
-rm -f key.pem
+rm -f $KEY_FILENAME
